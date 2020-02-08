a2495293faf2205953e9c676c6a219270ff0cdf4
David Hazell <david.james.hazell@gmail.com>
2020-02-08 13:50:26 -0500

    Created directory for test scripts

---
+++ test/test_position.py
@@ -0,0 +1,19 @@
+import time
+import sys
+
+sys.path.append('..')
+from src.motors.position import SwitchPosition
+
+interval_seconds = 5
+new_switch = SwitchPosition()
+
+print "DEBUG: Begin checking switch state"
+
+while True:
+
+    print "DEBUG: SWITCH STATE is %s" % new_switch.power_state
+    print "DEBUG: Checking state again in %s seconds..." % interval_seconds
+    time.sleep(interval_seconds)
+
+
+
